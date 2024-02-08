d9479c1e444ddf484daaa712c5dc28c6dc453d97
Lucas Vandenakker <90484710+lukeakker@users.noreply.github.com>
2024-02-08 12:35:36 -0500

    restaurant v2

--- restaurant.py
+++ restaurant.py
@@ -4,42 +4,71 @@
 
 #restaurant
 
+
+
 Joes = "Joe's Gourmet Burgers"
 Main = "Main Street Pizza Company"
 Corner = "Corner Café"
 Mamas = "Mama's Fine Italian"
 Chefs = "The Chef's Kitchen"
 
-vegetarian = input("Is anyone in your party vegetarian? ")
-vegan = input("Is anyone in your party vegan? ")
-glutenFree = input("Is anyone in your party gluten-free? ")
+repeat = True
+while(repeat == True):
+    
+    vegetarian = input("Is anyone in your party vegetarian? ")
 
-v = vegetarian
-ve = vegan
-g = glutenFree
-print("")
+    while(vegetarian != "yes" and vegetarian != "Yes" and vegetarian != "no" and vegetarian != "No"):
+        print("Not a valid input, try again: ")
+        vegetarian = input("Is anyone in your party vegetarian? ")
+        
+    vegan = input("Is anyone in your party vegan? ")
 
-if((v == "Yes" and ve == "Yes" and g == "No")or (v == "No" and ve == "Yes" and g == "Yes")or(v == "No" and ve == "Yes" and g == "No")or (v == "Yes" and ve == "Yes" and g == "Yes")):
-    print("Here are your restaurant choices.")
-    print(Corner)
-    print(Chefs)
-if(v == "No" and ve == "No" and g == "No"):
-    print("Here are your restaurant choices.")
-    print(Joes)
-    print(Mamas)
-    print(Main)
-    print(Corner)
-    print(Chefs)
-if(v == "Yes" and ve == "No" and g == "No"):
-    print("Here are your restaurant choices.")
-    print(Mamas)
-    print(Main)
-    print(Corner)
-    print(Chefs)
-if((v == "Yes" and ve == "No" and g == "Yes")or (v == "No" and ve == "No" and g == "Yes")):
-    print("Here are your restaurant choices.")
-    print(Main)
-    print(Corner)
-    print(Chefs)
+    while(vegan != "yes" and vegan != "Yes" and vegan != "No" and vegan != "no"):
+        print("Not a valid input, try again: ")
+        vegan = input("Is anyone in your party vegan? ")
+        
+    glutenFree = input("Is anyone in your party gluten-free? ")
+
+    while(glutenFree != "yes" and glutenFree != "Yes" and glutenFree != "no" and glutenFree !="No"):
+        print("Not a valid input, try again: ")
+        glutenFree = input("Is anyone in your party gluten-free? ")
+       
+
+    v = vegetarian
+    ve = vegan
+    g = glutenFree
+    print("")
+
+    if(((v == "Yes" or v =="yes") and (ve == "Yes" or ve == "yes") and (g == "No" or g == "no"))or ((v == "No" or v == "no") and (ve == "Yes" or ve == "yes") and (g == "Yes" or g == "yes"))or((v == "No" or v == "no") and (ve == "Yes" or ve == "yes") and (g == "No" or g =="no"))or ((v == "Yes" or v == "yes")and (ve == "Yes" or ve == "yes") and (g == "Yes" or g =="yes"))):
+        print("Here are your restaurant choices.")
+        print(Corner)
+        print(Chefs)
+    if((v == "No" or v == "no") and (ve == "No" or ve == "no") and (g == "No" or g == "no")):
+        print("Here are your restaurant choices.")
+        print(Joes)
+        print(Mamas)
+        print(Main)
+        print(Corner)
+        print(Chefs)
+    if((v == "Yes" or v == "yes") and (ve == "No" or ve== "no") and (g == "No" or g == "no")):
+        print("Here are your restaurant choices.")
+        print(Mamas)
+        print(Main)
+        print(Corner)
+        print(Chefs)
+    if(((v == "Yes" or v == "yes") and (ve == "No" or ve == "no") and (g == "Yes" or g =="yes"))or ((v == "No" or v == "no") and (ve == "No" or ve == "no") and (g == "Yes" or g == "yes"))):
+        print("Here are your restaurant choices.")
+        print(Main)
+        print(Corner)
+        print(Chefs)
+        
+    print("")
+    menuRepeat = input("Would you like to see the menu again?: ")
+    if(menuRepeat == "yes" or menuRepeat == "Yes"):
+        repeat = True
+    else:
+        repeat = False
+
+print("done")
 
 
